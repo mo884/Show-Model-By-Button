86f51f4a9daa4e590725cbd1d6f3d32cd5450ef6
mohamedessam77740@gmail.com <mohamedessam77740@gmail.com>
2023-11-19 20:54:33 +0200

    make model close by Escape key

--- JS/main.js
+++ JS/main.js
@@ -3,12 +3,21 @@ const modal= document.querySelector('.modal');
 const overlay =document.querySelector('.overlay');
 let btnCloseModal =document.querySelector('.close-modal');
 let btnOpenModel=document.querySelectorAll('.show-modal');
-//close Model When Press in close-modal
-const CloseModel = function(){
+
+//Function Close
+const CloseModel = function () {
     modal.classList.add('hidden');
-    overlay.classList.add('hidden'); 
+    overlay.classList.add('hidden');
 }
-btnCloseModal.addEventListener('click',CloseModel);
+//close Model When Press in close-modal
+btnCloseModal.addEventListener('click', CloseModel);
+//close Model When Press in Key
+document.addEventListener('keydown', function(e){
+    if(e.key==='Escape'&&!modal.classList.contains('hidden'))
+    CloseModel();
+});
+
+
 //Open Model When Press in show-modal
 const OpenModel = function(){
     modal.classList.remove('hidden');
